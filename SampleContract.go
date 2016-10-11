bbad0ea7cfc989671cffc39d566b242bc791355e
VercammenJo <jo.vercammen@in2scope.be>
2016-10-11 17:22:24 +0200

    commit 6

--- SampleContract.go
+++ SampleContract.go
@@ -3,13 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
-	"strconv"
-	"encoding/json"
 	"strings"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
-
+ // 	"strconv"  "encoding/json"
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
