0bfe5c6c4eb8b6f49b33d2f392d167a27435cfad
VercammenJo <jo.vercammen@in2scope.be>
2016-10-12 13:30:34 +0200

    commit final application

--- SampleContract.go
+++ SampleContract.go
@@ -57,12 +57,12 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 		return t.Init(stub, "init", args)
 	} else if function == "create_customer" {							   	//Creates a customer
 		return t.create_customer(stub, args)
-	//} else if function == "update_customer" {											//writes a value to the chaincode state
-	//	return t.update_customer(stub, args)
+	} else if function == "update_customer" {											//writes a value to the chaincode state
+		return t.update_customer(stub, args)
 	//} else if function == "init_Document" {									//create a new Document
 	//	return t.init_Document(stub, args)
-	//} else if function == "set_user" {										//change owner of a marble
-	//	return t.set_user(stub, args)
+	} else if function == "delete_customer" {										//change owner of a marble
+		return t.delete_customer(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
@@ -108,13 +108,75 @@ func (t *SimpleChaincode) read_customer(stub *shim.ChaincodeStub, args []string)
 }
 
 // ============================================================================================================================
+// Delete Customer - delete a customer from state 
+// ============================================================================================================================
+
+func (t *SimpleChaincode) delete_customer(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var name, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to delete")
+	}
+
+	name = args[0]
+	valAsbytes, err := stub.DelState(name)									//get the var from chaincode state
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil													//send it onward
+}
+
+
+// ============================================================================================================================
+// Update Customer - update a customer from state 
+// ============================================================================================================================
+
+func (t *SimpleChaincode) update_customer(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var err error
+
+	if len(args) != 5 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 5")
+	}
+
+	fmt.Println("- start update of customer")
+	if len(args[0]) <= 0 {
+		return nil, errors.New("1st argument must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return nil, errors.New("2nd argument must be a non-empty string")
+	}
+	if len(args[2]) <= 0 {
+		return nil, errors.New("3rd argument must be a non-empty string")
+	}
+	if len(args[3]) <= 0 {
+		return nil, errors.New("4th argument must be a non-empty string")
+	}
+	if len(args[4]) <= 0 {
+		return nil, errors.New("5th argument must be a non-empty string")
+	}
+	
+
+	str := `{"name": "` + args[0] + `", "surname": "` + args[1] + `", "address": "` + args[2]  + `, "city": "` + args[3]+  `, "country": "` + args[4]+ `"}`
+	err = stub.PutState(args[0], []byte(str))								//store marble with id as key
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("- end update customer")
+	return nil, nil													//send it onward
+}
+
+
+// ============================================================================================================================
 // Create Customer - create a customer in state 
 // ============================================================================================================================
 
 func (t *SimpleChaincode) create_customer(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var err error
-	//   0       1       2     3
-	// "asdf", "blue", "35", "bob"
+
 	if len(args) != 5 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 5")
 	}
